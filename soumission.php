a9e4281167e9cd38996b6005d7730ab82e761721
SAFA <sammoudasaffa@gmail.com>
2021-02-22 14:07:34 -0500

    installation composer

--- soumission.php
+++ soumission.php
@@ -11,14 +11,10 @@
  * Description: plugins des soumissions présentées aux Entreprises Écoles
  * Author: Sammouda Safa
  * Author URI: http://
- * Plugin URI: https://github.com/2ndkauboy/hello-world
+ * Plugin URI: 
  */
 
-// Hook the 'wp_footer' action hook, add the function named 'mfp_Add_Text' to it
-add_action("wp_footer", "mfp_Add_Text");
- 
-// Define 'mfp_Add_Text'
-function mfp_Add_Text()
-{
-  echo "<p style='color: black;'>After the footer is loaded, my text is added!</p>";
-}
+ defined('ABSPATH') or die ('not allowed');
+ if(file_exists(dirname(__FILE__).'/vendor/autoload.php')){
+   require_once dirname(__FILE__).'/vendor/autoload.php';
+ }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2d5d7d938cdd9ed310f19ffc93c0f0d6233c2e9e',
+    'name' => 'ecoles/soumissions',
+  ),
+  'versions' => 
+  array (
+    'ecoles/soumissions' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2d5d7d938cdd9ed310f19ffc93c0f0d6233c2e9e',
+    ),
+  ),
+);
